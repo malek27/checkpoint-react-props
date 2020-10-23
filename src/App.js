a2e59d405d499aa8888d9d836e9eebcc20e7b0a5
malek marzouki <marzouki.malek7M@gmail.com>
2020-10-23 13:54:11 +0100

    react-props2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Profile from './profile/Profile'
-import './App.css'
-import photo from "./profile/ProfilPhoto.jpg"
-import Alert from './profile/Alert'
+import React from "react";
+import Profile from "./profile/Profile";
+import "./App.css";
+import photo from "./profile/ProfilPhoto.jpg";
+import Alert from "./profile/Alert";
 function App() {
-  const styleObject={color: 'blue' , textAlign: 'center'};
-  const alertMyInput = name => alert(name);
- return (
-   <div style = {styleObject}>
-     <img src={photo} alt='pic'/>
-     <Profile profession='Future Developer'
-      bio='a 27 year-old web developer' 
-      fullName = 'Malek Marzouki'/>
-     < Alert name="Malek" alertMyInput={alertMyInput} />
-   </div>
+  const styleObject = { color: "blue", textAlign: "center" };
+  const alertMyInput = (name) => alert(name);
+  return (
+    <div style={styleObject}>
+      <Profile
+        profession="Future Developer"
+        bio="a 27 year-old web developer"
+        fullName="Malek Marzouki"
+      >
+        <img src={photo} alt="pic" />
+      </Profile>
+      <Alert name="Malek" alertMyInput={alertMyInput} />
+    </div>
   );
 }
 
